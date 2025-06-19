8017618be0dfa5f5989db6bfbbca6abbfc57970c
Fab0705 <117318115+Fab0705@users.noreply.github.com>
2025-06-18 23:16:46 -0500

    Parameters for Input Login

--- src/components/Buttons/InputField.jsx
+++ src/components/Buttons/InputField.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 
-export default function InputField({ label, type, placeholder }) {
+export default function InputField({ label, type, placeholder, maxlength, value, onChange }) {
   return (
     <div className="space-y-1">
-      <label className="text-sm font-medium">{label} <span className="text-red-500">*</span></label>
+      <label className="text-sm font-medium">
+        {label} <span className="text-red-500">*</span>
+      </label>
       <input
         type={type}
         placeholder={placeholder}
         className="w-full border rounded-md px-3 py-2 outline-none focus:ring-2 focus:ring-green-300"
+        maxlength={maxlength}
         required
+        value={value}
+        onChange={onChange}
       />
     </div>
   );
