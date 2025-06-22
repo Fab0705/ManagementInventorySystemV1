7fc57ac667391318d39776dbda2e377592e42731
Fab0705 <117318115+Fab0705@users.noreply.github.com>
2025-06-22 16:07:38 -0500

    Inventory Completed

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -5,6 +5,7 @@ import { MdModeEditOutline } from "react-icons/md";
 import Table from '../components/Table/Table';
 import Modal from '../components/Modals/Modal';
 import { getAllSpareParts, getSparePartById, createSparePart, updateSparePart, getSparePartsByLocation, updateStock } from '../services/sparePartService';
+import { getAllLocations, getAllRegions } from '../services/dataService';
 import { useAuth } from '../context/AuthContext';
 
 const theadText = ['Número de Parte', 'Descripción', 'Rework', 'Stock', 'Acciones'];
@@ -25,6 +26,10 @@ export default function Inventory() {
   const [searchTerm, setSearchTerm] = useState('');
   const [filteredSpareParts, setFilteredSpareParts] = useState([]);
 
+  const [locations, setLocations] = useState([]);
+  const [regions, setRegions] = useState([]);
+  const [selectedLocationId, setSelectedLocationId] = useState('');
+
   const { userData } = useAuth();
 
   const isAdmin = userData?.roles === 'Jefe de Logística';
@@ -38,21 +43,6 @@ export default function Inventory() {
     setIdSpare(null);
     setErrors({ numberPart: '', descPart: '', rework: '', sparePartStocks: '' });
   };
-
-  /* useEffect(() => {
-    const fetchSpareParts = async () => {
-      try {
-        const data = await getAllSpareParts();
-        setSpareParts(Array.isArray(data) ? data : []); // asignar los datos al estado
-      } catch (error) {
-        console.error('Error fetching spare parts:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchSpareParts();
-  }, []); */
 
   useEffect(() => {
     const fetchSpareParts = async () => {
@@ -78,16 +68,42 @@ export default function Inventory() {
   }, [userData]);
 
   useEffect(() => {
-    if (searchTerm.trim() === '') {
-      setFilteredSpareParts(spareParts);
-    } else {
+    let filtered = [...spareParts];
+
+    if (selectedLocationId) {
+      filtered = filtered.filter(sp =>
+        sp.sparePartStocks?.some(stock => stock.idLoc === selectedLocationId)
+      );
+
+      console.log(filtered);
+    }
+
+    if (searchTerm.trim() !== '') {
       const lowerTerm = searchTerm.toLowerCase();
-      const filtered = spareParts.filter(sp =>
+      filtered = filtered.filter(sp =>
         sp.numberPart.toLowerCase().includes(lowerTerm)
       );
-      setFilteredSpareParts(filtered);
     }
-  }, [searchTerm, spareParts]);
+
+    setFilteredSpareParts(filtered);
+  }, [searchTerm, spareParts, selectedLocationId]);
+
+  useEffect(() => {
+    const fetchLocationsAndRegions = async () => {
+      try {
+        const [locData, regData] = await Promise.all([
+          getAllLocations(),
+          getAllRegions()
+        ]);
+        setLocations(locData);
+        setRegions(regData);
+      } catch (err) {
+        console.error("Error al obtener locaciones y regiones:", err);
+      }
+    };
+
+    if (isAdmin) fetchLocationsAndRegions();
+  }, [isAdmin]);
 
   const openEditModal = async (sparePart) => {
     const data = await getSparePartById(sparePart.idSpare);
@@ -168,28 +184,12 @@ export default function Inventory() {
       <td>{item.descPart}</td>
       <td>{item.rework ? 'Yes' : 'No'}</td>
       <td>
-        {item.sparePartStocks && item.sparePartStocks.length > 0
-          ? item.sparePartStocks.reduce((acc, stock) => acc + stock.quantity, 0)
-          : 'No stock'}
-      </td>
-      {/* 
-      Para el filtrado
-      <td>
-        {item.sparePartStocks && item.sparePartStocks.length > 0 ? (
-          <>
-            <div><strong>Total:</strong> {item.sparePartStocks.reduce((acc, stock) => acc + stock.quantity, 0)}</div>
-            <div className="text-xs text-gray-500 mt-1">
-              {item.sparePartStocks
-                .filter(stock => stock.idLoc === selectedLocationId)
-                .map((stock, i) => (
-                  <div key={i}>
-                    Solo en ubicación {stock.idLoc}: {stock.quantity}
-                  </div>
-                ))}
-            </div>
-          </>
-        ) : 'No stock'}
-      </td> */}
+          {item.sparePartStocks && item.sparePartStocks.length > 0 ? (
+            selectedLocationId
+              ? item.sparePartStocks.find(s => s.idLoc === selectedLocationId)?.quantity ?? '0'
+              : item.sparePartStocks.reduce((acc, stock) => acc + stock.quantity, 0)
+          ) : 'No stock'}
+      </td>      
       <td>
         {!isAdmin && (
           <ButtonAction
@@ -214,7 +214,7 @@ export default function Inventory() {
           <hr />
 
           <div className="flex flex-row gap-4">
-            {/* Número de Parte */}
+            
             <div className="flex-1">
               <label className="block font-bold italic mb-1">Número de Parte</label>
               <input
@@ -230,7 +230,6 @@ export default function Inventory() {
               )}
             </div>
 
-            {/* Cantidad en stock */}
             <div className="w-40">
               <label className="block font-bold italic mb-1">Cantidad en stock</label>
               <input
@@ -247,8 +246,6 @@ export default function Inventory() {
             </div>
           </div>
           
-
-          {/* Campo: Descripción */}
           <label className='block font-bold italic mt-3'>Descripción</label>
           <textarea
             value={descPart}
@@ -264,9 +261,6 @@ export default function Inventory() {
           </div>
           {errors.descPart && <p className="text-sm text-red-500">{errors.descPart}</p>}
 
-          
-
-          {/* Campo: Rework */}
           <div className="flex items-center gap-2 mt-3">
             <input
               type="checkbox"
@@ -289,9 +283,30 @@ export default function Inventory() {
 
       <div className="bg-gray-100 w-full h-dvh p-6 overflow-y-auto">
         <h1 className="text-xl font-bold mb-6">Repuestos</h1>
+        <div className="flex justify-between items-center mb-4 gap-4">
+          <input
+            className="border px-3 py-2 rounded w-1/3"
+            placeholder="Buscar por número de parte..."
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          
+          {isAdmin && (
+            <select
+              value={selectedLocationId}
+              onChange={(e) => setSelectedLocationId(e.target.value)}
+              className="border px-3 py-2 rounded"
+            >
+              <option value="">Todas las ubicaciones</option>
+              {locations.map(loc => (
+                <option key={loc.idLoc} value={loc.idLoc}>
+                  {
+                    `${regions.find(r => r.idReg === loc.idReg)?.descReg || 'Región desconocida'}`
+                  }
+                </option>
+              ))}
+            </select>
+          )}
 
-        <div className="flex justify-between items-center mb-4">
-          <input className="border px-3 py-2 rounded w-1/3" placeholder="Buscar por número de parte..." onChange={(e) => setSearchTerm(e.target.value)} />
           {!isAdmin && (
             <Button
               children={"Agregar Repuesto"}
